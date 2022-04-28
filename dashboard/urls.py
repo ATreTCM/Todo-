6f3503fcac83e225ebd944d37a097205e37d951b
ATreTCM <atret1988@gmail.com>
2022-04-28 20:41:44 +0300

    urls 0 and views 0

---
+++ dashboard/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'dashboard'
+
+urlpatterns = [
+    path('<int:pk>/update', views.TdUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete', views.TdDeleteView.as_view(), name='pay_delete'),
+    path('', views.TdListView.as_view(), name='list'),
+    path('<int:pk>', views.TdDetailView.as_view(), name='detail'),
+
+]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.views.generic import UpdateView, DetailView, DeleteView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from .models import TodoDb
+
+
+from .forms import TdForm
+
+
+class TdListView(LoginRequiredMixin, ListView):
+    model = TodoDb
+    template_name = 'dashboard/list.html'
+    context_object_name = 'list_content'
+    raise_exception = True
+
+
+class TdDetailView(LoginRequiredMixin, DetailView):
+    model = TodoDb
+    template_name = 'dashboard/details.html'
+    context_object_name = 'detail_content'
+    raise_exception = True
+
+
+class TdUpdateView(LoginRequiredMixin, UpdateView):
+    model = TodoDb
+    template_name = 'dashboard/update.html'
+    fields = ['author',
+              'header',
+              'content',
+              'slave',
+              'photo',
+              ]
+    raise_exception = True
+
+
+class TdDeleteView(LoginRequiredMixin, DeleteView):
+    model = TodoDb
+    success_url = '/'
+    template_name = 'dashboard/delete.html'
+    raise_exception = True
+
+
